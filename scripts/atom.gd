41fa41c6c298acbda9c5af2a3e5f9060a1bb3892
Ayush Kumar <ayush.k@outlook.com>
2024-07-21 10:44:29 -0400

    Reimplemented force interactions similar to ParticleLife. Can see clustering of different-colored particles, but not enough to show anything definitive. Need to fine-tune force model furhter.

--- scripts/atom.gd
+++ scripts/atom.gd
@@ -1,26 +1,24 @@
+class_name Atom
 extends Node2D
-
-@export var atom_charge = -2
-@export var atom_mass = 15.9 # AMU
-@export var atom_radius = 10
-@export var atom_color = Color.BLACK
-var atom_velocity = Vector2.ZERO
 
 @onready var screen_size = get_viewport_rect().size
 
+@export var type = 0
+@export var mass = 1
+@export var radius = 10
+@export var color = Color.BLACK
+@export var velocity = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	queue_redraw()
 	screen_wrap()
 
 func _draw():
-	draw_circle(Vector2.ZERO, atom_radius, atom_color)
+	draw_circle(Vector2.ZERO, radius, color)
 
 func screen_wrap():
 	position.x = wrapf(position.x, -20, screen_size.x + 20)
--- scripts/atom_manager.gd
+++ scripts/atom_manager.gd
@@ -1,47 +1,64 @@
 extends Node2D
 
-var Atom = preload("res://scenes/atom.tscn")
-var atoms_list = []
-const COULOMB_CONSTANT = 1e6
+@onready var screen_size : Vector2 = get_viewport_rect().size
+const atom_class = preload("res://scenes/atom.tscn")
+var atoms_list : Array[Atom] = []
+var num_type_atoms : int = 6
+var num_atoms : int = 100
+var type_atoms_colors : Array[Color] = [Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PURPLE]
+var force_matrix : Array = []
+var min_dist_repulsion : float = 20.0
+var max_dist_influence : float = 300.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+func _ready() -> void:
+	initialize_force_matrix()
+	spawn_atoms()
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	if len(atoms_list) != 0:
-		for atom1 in atoms_list:
-			var net_force = Vector2.ZERO
-			for atom2 in atoms_list:
-				if atom1 != atom2:
-					net_force += calculate_force(atom1, atom2)
+		for atom : Atom in atoms_list:
+			var net_force : Vector2 = Vector2.ZERO
+			for other_atom : Atom in atoms_list:
+				if atom != other_atom:
+					net_force += calculate_force_linear(atom, other_atom)
 			
-			atom1.atom_velocity += (net_force / atom1.atom_mass) * delta
-			atom1.position += atom1.atom_velocity * delta  # Update the position directly
-			
-			#print("Atom position: ", atom1.position)
-			#print("Atom velocity: ", atom1.atom_velocity)
-		
-func calculate_force(atom1, atom2):
-	# Calculating scalar distances and direction from atom1 to atom2
-	var distance = atom2.position - atom1.position
-	var distance_squared = distance.length_squared()
-	var direction = distance.normalized()
-	
-	if distance_squared < 10:
-		return Vector2.ZERO
-	
-	var force_magnitude = (-1) * COULOMB_CONSTANT * (atom1.atom_charge * atom2.atom_charge) / distance_squared
-	
-	return direction * force_magnitude
+			atom.velocity += 0.95 * (net_force / atom.mass) * delta
+			atom.position += atom.velocity * delta  # Update the position directly
 
-# Checks if user left-clicks mouse, and if so, creates a new atom
-# at the mouse location.
-func _unhandled_input(event):
-	if event.is_action_pressed("left_click"):
-		var click_position = get_global_mouse_position()
-		var atom = Atom.instantiate()
-		atom.position = click_position
-		atoms_list.append(atom)
+# Initalizes force, min_dist, and max_dist matrices
+func initialize_force_matrix() -> void:
+	force_matrix.resize(num_type_atoms)
+	
+	for i : int in range(num_type_atoms):
+		force_matrix[i] = []
+		for j : int in range(num_type_atoms):
+			force_matrix[i].append(max(randf_range(-1, -0.3), randf_range(0.3, 1)))
+
+func spawn_atoms() -> void:
+	for i : int in range(num_atoms):
+		var atom : Atom = atom_class.instantiate()
+		atom.type = randi() % num_type_atoms  # randi_range(0, num_type_atoms)
+		atom.color = type_atoms_colors[atom.type]
+		atom.position = Vector2(randf() * screen_size.x, randf() * screen_size.y)
 		add_child(atom)
-		get_viewport().set_input_as_handled()
+		atoms_list.append(atom)
+	
+func calculate_force_linear(atom : Atom, other_atom : Atom) -> Vector2:
+	var total_force : Vector2 = Vector2(0, 0)
+	
+	var distance : Vector2 = atom.position - other_atom.position
+	var distance_magnitude : float = distance.length()
+	var direction : Vector2 = distance.normalized()
+	
+	# If atoms are in the repulsive region, then add a repulsive force
+	# so that the atoms do not overlap.
+	if distance_magnitude < min_dist_repulsion:
+		total_force += direction * 1.5 * abs(force_matrix[atom.type][other_atom.type]) * distance_magnitude / min_dist_repulsion
+	
+	# If atoms are less than their degree of influence, then apply
+	# the primary force.
+	if distance_magnitude < max_dist_influence:
+		total_force += (-1) * direction * force_matrix[atom.type][other_atom.type] * distance_magnitude / max_dist_influence
+	
+	return total_force
